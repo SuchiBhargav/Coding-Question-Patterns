906ff93766c1b0520159e83acac4af9e314ad62f
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-27 15:24:21 +0300

    Solved - Problem Challenge 4 - Words Concatenation (hard)

--- 1. Pattern Sliding Window/Problem Challenge 4 - Words Concatenation (hard) .py
+++ 1. Pattern Sliding Window/Problem Challenge 4 - Words Concatenation (hard) .py
@@ -17,77 +17,85 @@ Output: [3]
 Explanation: The only substring containing both the words is "catfox".
 '''
 
-#mycode
+
+# mycode
+import collections
+
+
 def find_word_concatenation(str, words):
-  result_indices = []
-  word_count = len(words)
-  word_len=len(words[0])
+    if len(words) == 0 or len(words[0]) == 0:
+        return []
 
-  for i in range(len(str)-word_count*word_len+1):
-    cnt = 0
-    curr=str[i:i+word_count*word_len]
-    
-    for j in range(word_count):
-      
-      if words[j] not in curr:
-        break
-      else:
-        cnt += 1
-    
-    if cnt== word_count:
-      result_indices.append(i)
-      
-  return result_indices
+    word_frequency = collections.Counter(words)
+
+    result_indices = []
+    words_count = len(words)
+    word_length = len(words[0])
+
+    for i in range((len(str) - words_count * word_length) + 1):
+        words_seen = collections.defaultdict(int)
+        for j in range(words_count):
+            next_word_index = i + j * word_length
+            # Get the next word from the string
+            word = str[next_word_index: next_word_index + word_length]
+            if word in word_frequency:
+                # Add the word to the 'words_seen' map
+                words_seen[word] += 1
+
+                # No need to process further if the word has higher frequency than required
+                if words_seen[word] <= word_frequency.get(word, 0):
+                    if j + 1 == words_count:  # Store index if we have found all the words
+                        result_indices.append(i)
+
+    return result_indices
 
 
-
-#answer
+# answer
 def find_word_concatenation(str, words):
-  if len(words) == 0 or len(words[0]) == 0:
-    return []
+    if len(words) == 0 or len(words[0]) == 0:
+        return []
 
-  word_frequency = {}
+    word_frequency = {}
 
-  for word in words:
-    if word not in word_frequency:
-      word_frequency[word] = 0
-    word_frequency[word] += 1
+    for word in words:
+        if word not in word_frequency:
+            word_frequency[word] = 0
+        word_frequency[word] += 1
 
-  result_indices = []
-  words_count = len(words)
-  word_length = len(words[0])
+    result_indices = []
+    words_count = len(words)
+    word_length = len(words[0])
 
-  for i in range((len(str) - words_count * word_length)+1):
-    words_seen = {}
-    for j in range(0, words_count):
-      next_word_index = i + j * word_length
-      # Get the next word from the string
-      word = str[next_word_index: next_word_index + word_length]
-      if word not in word_frequency:  # Break if we don't need this word
-        break
+    for i in range((len(str) - words_count * word_length) + 1):
+        words_seen = {}
+        for j in range(0, words_count):
+            next_word_index = i + j * word_length
+            # Get the next word from the string
+            word = str[next_word_index: next_word_index + word_length]
+            if word not in word_frequency:  # Break if we don't need this word
+                break
 
-      # Add the word to the 'words_seen' map
-      if word not in words_seen:
-        words_seen[word] = 0
-      words_seen[word] += 1
+            # Add the word to the 'words_seen' map
+            if word not in words_seen:
+                words_seen[word] = 0
+            words_seen[word] += 1
 
-      # No need to process further if the word has higher frequency than required
-      if words_seen[word] > word_frequency.get(word, 0):
-        break
+            # No need to process further if the word has higher frequency than required
+            if words_seen[word] > word_frequency.get(word, 0):
+                break
 
-      if j + 1 == words_count:  # Store index if we have found all the words
-        result_indices.append(i)
+            if j + 1 == words_count:  # Store index if we have found all the words
+                result_indices.append(i)
 
-  return result_indices
+    return result_indices
 
 
 def main():
-  print(find_word_concatenation("catfoxcat", ["cat", "fox"]))
-  print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
+    print(find_word_concatenation("catfoxcat", ["cat", "fox"]))
+    print(find_word_concatenation("catcatfoxfox", ["cat", "fox"]))
 
 
 main()
-
 
 '''
 Time Complexity 
