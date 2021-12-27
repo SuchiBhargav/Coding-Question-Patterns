2015e093453d66e4c25b5798d5aa7d1148b2a4ae
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-27 14:16:16 +0300

    Solved - Problem Challenge 3 - Smallest Window containing Substring (hard)

--- 1. Pattern Sliding Window/Problem Challenge 3 - Smallest Window containing Substring (hard).py
+++ 1. Pattern Sliding Window/Problem Challenge 3 - Smallest Window containing Substring (hard).py
@@ -23,96 +23,84 @@ Output: ""
 Explanation: No substring in the given string has all characters of the pattern.
 '''
 
+import collections
 
-#mycode
-import math
+
+# mycode
 def find_substring(str, pattern):
-  # TODO: Write your code here
-  p_dict={}
-  s_dict={}
-  result=""
+    window_start, matched, substr_start = 0, 0, 0
+    min_length = len(str) + 1
+    char_frequency = collections.Counter(pattern)
 
-  win_start, min_len = 0, math.inf
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char in char_frequency:
+            char_frequency[right_char] -= 1
+            if char_frequency[right_char] >= 0:  # count as match only if it is not duplicate
+                matched += 1
 
-  for p in pattern:
-    if p not in p_dict:
-      p_dict[p] = 1
-    else:
-      p_dict[p] += 1
+        # Shrink the window if we can, finish as soon as we remove a matched character
+        while matched == len(pattern):
+            if min_length > window_end - window_start + 1:
+                min_length = window_end - window_start + 1
+                substr_start = window_start
 
-  for win_end in range(len(str)):
-    if str[win_end] not in s_dict:
-      s_dict[str[win_end]] = 1
-    else:
-      s_dict[str[win_end]] += 1
-    
-    print(s_dict)
+            left_char = str[window_start]
+            if left_char in char_frequency:
+                if char_frequency[left_char] == 0:  # don't decrement when duplicate
+                    matched -= 1
+                char_frequency[left_char] += 1
+            window_start += 1
 
-    while set(p_dict.keys()).issubset(set(s_dict.keys())):
-      if win_end-win_start+1 < min_len:
-        min_len=win_end-win_start+1
-        if win_end == len(str)-1:
-          result=str[win_start:]
-        else:
-          result=str[win_start:win_end+1]
-
-      if s_dict[str[win_start]] == 1:
-        del s_dict[str[win_start]]
-      else:
-        s_dict[str[win_start]] -= 1
-      win_start += 1
-
-  return result
+    return str[substr_start:substr_start + min_length] if min_length <= len(str) else ""
 
 
-
-#answer
+# answer
 def find_substring(str, pattern):
-  window_start, matched, substr_start = 0, 0, 0
-  min_length = len(str) + 1
-  char_frequency = {}
+    window_start, matched, substr_start = 0, 0, 0
+    min_length = len(str) + 1
+    char_frequency = {}
 
-  for chr in pattern:
-    if chr not in char_frequency:
-      char_frequency[chr] = 0
-    char_frequency[chr] += 1
+    for chr in pattern:
+        if chr not in char_frequency:
+            char_frequency[chr] = 0
+        char_frequency[chr] += 1
 
-  # try to extend the range [window_start, window_end]
-  for window_end in range(len(str)):
-    right_char = str[window_end]
-    if right_char in char_frequency:
-      char_frequency[right_char] -= 1
-      if char_frequency[right_char] >= 0:  # Count every matching of a character
-        matched += 1
+    # try to extend the range [window_start, window_end]
+    for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char in char_frequency:
+            char_frequency[right_char] -= 1
+            if char_frequency[right_char] >= 0:  # Count every matching of a character
+                matched += 1
 
-    # Shrink the window if we can, finish as soon as we remove a matched character
-    while matched == len(pattern):
-      if min_length > window_end - window_start + 1:
-        min_length = window_end - window_start + 1
-        substr_start = window_start
+        # Shrink the window if we can, finish as soon as we remove a matched character
+        while matched == len(pattern):
+            if min_length > window_end - window_start + 1:
+                min_length = window_end - window_start + 1
+                substr_start = window_start
 
-      left_char = str[window_start]
-      window_start += 1
-      if left_char in char_frequency:
-        # Note that we could have redundant matching characters, therefore we'll decrement the
-        # matched count only when a useful occurrence of a matched character is going out of the window
-        if char_frequency[left_char] == 0:
-          matched -= 1
-        char_frequency[left_char] += 1
+            left_char = str[window_start]
+            window_start += 1
+            if left_char in char_frequency:
+                # Note that we could have redundant matching characters, therefore we'll decrement the
+                # matched count only when a useful occurrence of a matched character is going out of the window
+                if char_frequency[left_char] == 0:
+                    matched -= 1
+                char_frequency[left_char] += 1
 
-  if min_length > len(str):
-    return ""
-  return str[substr_start:substr_start + min_length]
+    if min_length > len(str):
+        return ""
+    return str[substr_start:substr_start + min_length]
 
 
 def main():
-  print(find_substring("aabdec", "abc"))
-  print(find_substring("abdabca", "abc"))
-  print(find_substring("adcad", "abc"))
+    print(find_substring("aabdec", "abc"))
+    print(find_substring("abdabca", "abc"))
+    print(find_substring("adcad", "abc"))
+
 
 main()
-
-
 
 '''
 Time Complexity 
