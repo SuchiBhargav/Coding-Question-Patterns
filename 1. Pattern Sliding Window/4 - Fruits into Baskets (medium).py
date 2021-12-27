2e60f683be4b439d9c1a4dad33de3082a8440ae1
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-27 12:37:47 +0300

    Solved 7 Sliding Window Questions

--- 1. Pattern Sliding Window/4 - Fruits into Baskets (medium).py
+++ 1. Pattern Sliding Window/4 - Fruits into Baskets (medium).py
@@ -20,62 +20,61 @@ Explanation: We can put 3 'B' in one basket and two 'C' in the other basket.
 This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
 '''
 
-#mycode
+
+# mycode
+import collections
+
+
 def fruits_into_baskets(fruits):
-  max_len, win_start = 0, 0
-  dict_fruits={}
+    max_len, win_start = 0, 0
+    dict_fruits = collections.defaultdict(int)
 
-  for win_end in range(len(fruits)):
-    if fruits[win_end] not in dict_fruits:
-      dict_fruits[fruits[win_end]] = 1
-    else: 
-      dict_fruits[fruits[win_end]] += 1
-    
-    while len(dict_fruits) >2:
-      if dict_fruits[fruits[win_start]] == 1:
-        del dict_fruits[fruits[win_start]]
-      else:
-        dict_fruits[fruits[win_start]] -= 1
-      win_start += 1 
+    for win_end in range(len(fruits)):
+        dict_fruits[fruits[win_end]] += 1
 
-    if len(dict_fruits) == 2:
-      max_len=max(max_len, sum(dict_fruits.values()))
-      
-  return max_len
+        while len(dict_fruits) > 2:
+            if dict_fruits[fruits[win_start]] == 1:
+                del dict_fruits[fruits[win_start]]
+            else:
+                dict_fruits[fruits[win_start]] -= 1
+            win_start += 1
+
+        if len(dict_fruits) == 2:
+            max_len = max(max_len, sum(dict_fruits.values()))
+
+    return max_len
 
 
-#answer
+# answer
 def fruits_into_baskets(fruits):
-  window_start = 0
-  max_length = 0
-  fruit_frequency = {}
+    window_start = 0
+    max_length = 0
+    fruit_frequency = {}
 
-  # try to extend the range [window_start, window_end]
-  for window_end in range(len(fruits)):
-    right_fruit = fruits[window_end]
-    if right_fruit not in fruit_frequency:
-      fruit_frequency[right_fruit] = 0
-    fruit_frequency[right_fruit] += 1
+    # try to extend the range [window_start, window_end]
+    for window_end in range(len(fruits)):
+        right_fruit = fruits[window_end]
+        if right_fruit not in fruit_frequency:
+            fruit_frequency[right_fruit] = 0
+        fruit_frequency[right_fruit] += 1
 
-    # shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
-    while len(fruit_frequency) > 2:
-      left_fruit = fruits[window_start]
-      fruit_frequency[left_fruit] -= 1
-      if fruit_frequency[left_fruit] == 0:
-        del fruit_frequency[left_fruit]
-      window_start += 1  # shrink the window
-    max_length = max(max_length, window_end-window_start + 1)
-  return max_length
+        # shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
+        while len(fruit_frequency) > 2:
+            left_fruit = fruits[window_start]
+            fruit_frequency[left_fruit] -= 1
+            if fruit_frequency[left_fruit] == 0:
+                del fruit_frequency[left_fruit]
+            window_start += 1  # shrink the window
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
 def main():
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
-  print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
+    print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'A', 'C'])))
+    print("Maximum number of fruits: " + str(fruits_into_baskets(['A', 'B', 'C', 'B', 'B', 'C'])))
 
 
 main()
-
-
 
 '''
 Time Complexity 
@@ -92,4 +91,3 @@ Given a string, find the length of the longest substring in it with at most two 
 
 Solution: This problem is exactly similar to our parent problem.
 '''
-
