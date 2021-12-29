1b5f809b8129c9bdda5d26c09e415792c782e408
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-29 11:15:31 +0300

    Completed Linkedlist Reversal Questions

--- 06. Pattern In-place Reversal of a LinkedList/3 - Reverse every K-element Sub-list (medium).py
+++ 06. Pattern In-place Reversal of a LinkedList/3 - Reverse every K-element Sub-list (medium).py
@@ -1,15 +1,14 @@
 '''
-Problem Challenge 1
-
-Reverse alternating K-element Sub-list (medium)
-
-Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
+Problem Statement 
+Given the head of a LinkedList and a number ‘k’, reverse every ‘k’ sized sub-list starting from the head.
 
 If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
 '''
 
-
 # mycode
+from __future__ import print_function
+
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
@@ -23,7 +22,7 @@ class Node:
         print()
 
 
-def reverse_alternate_k_elements(head, k):
+def reverse_every_k_elements(head, k):
     if k <= 1 or head is None:
         return head
 
@@ -32,84 +31,8 @@ def reverse_alternate_k_elements(head, k):
         last_node_of_previous_part = previous
         last_node_of_sub_list = current
         next = None
-
-        # reverse 'k' nodes
         i = 0
-        while current and i < k:
-            next = current.next
-            current.next = previous
-            previous = current
-            current = next
-            i += 1
-
-        # connect with the previous part
-        if last_node_of_previous_part:
-            last_node_of_previous_part.next = previous
-        else:
-            head = previous
-
-        # connect with the next part
-        last_node_of_sub_list.next = current
-
-        # skip 'k' nodes
-        i = 0
-        while current and i < k:
-            previous = current
-            current = current.next
-            i += 1
-
-        if current is None:
-            break
-    return head
-
-
-def main():
-    head = Node(1)
-    head.next = Node(2)
-    head.next.next = Node(3)
-    head.next.next.next = Node(4)
-    head.next.next.next.next = Node(5)
-    head.next.next.next.next.next = Node(6)
-    head.next.next.next.next.next.next = Node(7)
-    head.next.next.next.next.next.next.next = Node(8)
-
-    print("Nodes of original LinkedList are: ", end='')
-    head.print_list()
-    result = reverse_alternate_k_elements(head, 2)
-    print("Nodes of reversed LinkedList are: ", end='')
-    result.print_list()
-
-
-main()
-
-# answer
-class Node:
-    def __init__(self, value, next=None):
-        self.value = value
-        self.next = next
-
-    def print_list(self):
-        temp = self
-        while temp is not None:
-            print(temp.value, end=" ")
-            temp = temp.next
-        print()
-
-
-def reverse_alternate_k_elements(head, k):
-    if k <= 1 or head is None:
-        return head
-
-    current, previous = head, None
-    while True:
-        last_node_of_previous_part = previous
-        # after reversing the LinkedList 'current' will become the last node of the sub-list
-        last_node_of_sub_list = current
-        next = None  # will be used to temporarily store the next node
-
-        # reverse 'k' nodes
-        i = 0
-        while current is not None and i < k:
+        while current and i < k:  # reverse 'k' nodes
             next = current.next
             current.next = previous
             previous = current
@@ -125,15 +48,9 @@ def reverse_alternate_k_elements(head, k):
         # connect with the next part
         last_node_of_sub_list.next = current
 
-        # skip 'k' nodes
-        i = 0
-        while current is not None and i < k:
-            previous = current
-            current = current.next
-            i += 1
-
         if current is None:
             break
+        previous = last_node_of_sub_list
     return head
 
 
@@ -149,7 +66,74 @@ def main():
 
     print("Nodes of original LinkedList are: ", end='')
     head.print_list()
-    result = reverse_alternate_k_elements(head, 2)
+    result = reverse_every_k_elements(head, 3)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+main()
+
+
+# answer
+# class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+def reverse_every_k_elements(head, k):
+    if k <= 1 or head is None:
+        return head
+
+    current, previous = head, None
+    while True:
+        last_node_of_previous_part = previous
+        # after reversing the LinkedList 'current' will become the last node of the sub-list
+        last_node_of_sub_list = current
+        next = None  # will be used to temporarily store the next node
+        i = 0
+        while current is not None and i < k:  # reverse 'k' nodes
+            next = current.next
+            current.next = previous
+            previous = current
+            current = next
+            i += 1
+
+        # connect with the previous part
+        if last_node_of_previous_part is not None:
+            last_node_of_previous_part.next = previous
+        else:
+            head = previous
+
+        # connect with the next part
+        last_node_of_sub_list.next = current
+
+        if current is None:
+            break
+        previous = last_node_of_sub_list
+    return head
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse_every_k_elements(head, 3)
     print("Nodes of reversed LinkedList are: ", end='')
     result.print_list()
 
