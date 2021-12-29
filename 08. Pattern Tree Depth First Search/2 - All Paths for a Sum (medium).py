474a89849fb3b197226c04887eba849efc31c359
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-29 13:56:03 +0300

    Completed Depth First Search Questions

--- 08. Pattern Tree Depth First Search/2 - All Paths for a Sum (medium).py
+++ 08. Pattern Tree Depth First Search/2 - All Paths for a Sum (medium).py
@@ -5,106 +5,100 @@ find all paths from root-to-leaf such that the sum of all the node values of eac
 '''
 
 
-#mycode
+# mycode
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_paths(root, sum):
-  allPaths = []
-  # TODO: Write your code here
+    allPaths = []
+    find_current_paths(root, sum, [], allPaths)
+    return allPaths
 
-  find_current_paths(root, sum,[], allPaths)
-  return allPaths
 
 def find_current_paths(currentNode, sum, currentPath, allPaths):
-  if not currentNode:
-    return
-  
-  currentPath.append(currentNode.val)
-  
+    if not currentNode:
+        return
 
-  if currentNode.val == sum and currentNode.left is None and currentNode.right is None:
-    allPaths.append(list(currentPath))
-  else:
-    find_current_paths(currentNode.left, sum - currentNode.val, currentPath, allPaths)
-    find_current_paths(currentNode.right, sum - currentNode.val, currentPath, allPaths)
-  
-  del currentPath[-1]
+    currentPath.append(currentNode.val)
+
+    if currentNode.val == sum and currentNode.left is None and currentNode.right is None:
+        allPaths.append(list(currentPath))
+    else:
+        find_current_paths(currentNode.left, sum - currentNode.val, currentPath, allPaths)
+        find_current_paths(currentNode.right, sum - currentNode.val, currentPath, allPaths)
+
+    del currentPath[-1]
 
 
 def main():
-
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(4)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  sum = 23
-  print("Tree paths with sum " + str(sum) +
-        ": " + str(find_paths(root, sum)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    sum = 23
+    print("Tree paths with sum " + str(sum) +
+          ": " + str(find_paths(root, sum)))
 
 
 main()
 
 
-#answer
+# answer
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 def find_paths(root, sum):
-  allPaths = []
-  find_paths_recursive(root, sum, [], allPaths)
-  return allPaths
+    allPaths = []
+    find_paths_recursive(root, sum, [], allPaths)
+    return allPaths
 
 
 def find_paths_recursive(currentNode, sum, currentPath, allPaths):
-  if currentNode is None:
-    return
+    if currentNode is None:
+        return
 
-  # add the current node to the path
-  currentPath.append(currentNode.val)
+    # add the current node to the path
+    currentPath.append(currentNode.val)
 
-  # if the current node is a leaf and its value is equal to sum, save the current path
-  if currentNode.val == sum and currentNode.left is None and currentNode.right is None:
-    allPaths.append(list(currentPath))
-  else:
-    # traverse the left sub-tree
-    find_paths_recursive(currentNode.left, sum -
-                         currentNode.val, currentPath, allPaths)
-    # traverse the right sub-tree
-    find_paths_recursive(currentNode.right, sum -
-                         currentNode.val, currentPath, allPaths)
+    # if the current node is a leaf and its value is equal to sum, save the current path
+    if currentNode.val == sum and currentNode.left is None and currentNode.right is None:
+        allPaths.append(list(currentPath))
+    else:
+        # traverse the left sub-tree
+        find_paths_recursive(currentNode.left, sum -
+                             currentNode.val, currentPath, allPaths)
+        # traverse the right sub-tree
+        find_paths_recursive(currentNode.right, sum -
+                             currentNode.val, currentPath, allPaths)
 
-  # remove the current node from the path to backtrack,
-  # we need to remove the current node while we are going up the recursive call stack.
-  del currentPath[-1]
+    # remove the current node from the path to backtrack,
+    # we need to remove the current node while we are going up the recursive call stack.
+    del currentPath[-1]
 
 
 def main():
-
-  root = TreeNode(12)
-  root.left = TreeNode(7)
-  root.right = TreeNode(1)
-  root.left.left = TreeNode(4)
-  root.right.left = TreeNode(10)
-  root.right.right = TreeNode(5)
-  sum = 23
-  print("Tree paths with sum " + str(sum) +
-        ": " + str(find_paths(root, sum)))
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    sum = 23
+    print("Tree paths with sum " + str(sum) +
+          ": " + str(find_paths(root, sum)))
 
 
 main()
-
-
 
 '''
 Time complexity 
@@ -136,7 +130,6 @@ From the above discussion, we can conclude that the overall space complexity of 
 Also from the above discussion, since for each leaf node, in the worst case, 
 we have to copy log(N) nodes to store its path, therefore the time complexity of our algorithm will also be O(N*logN).
 '''
-
 
 '''
 Similar Problems 
--- 08. Pattern Tree Depth First Search/Problem Challenge 1 - Tree Diameter (medium).py
+++ 08. Pattern Tree Depth First Search/Problem Challenge 1 - Tree Diameter (medium).py
@@ -10,117 +10,105 @@ The diameter of a tree may or may not pass through the root.
 Note: You can always assume that there are at least two leaf nodes in the given tree.
 '''
 
-#mycode
+
+# mycode
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class TreeDiameter:
 
-  def __init__(self):
-    self.treeDiameter = 0
+    def __init__(self):
+        self.treeDiameter = 0
 
-  def find_diameter(self, root):
-    # TODO: Write your code here
-    def depth(currentNode):
-      if not currentNode:
-        return 0
-      
-      left = depth(currentNode.left)
-      right = depth(currentNode.right)
+    def find_diameter(self, root):
+        def depth(currentNode):
+            if not currentNode:
+                return 0
 
-      self.treeDiameter = max(self.treeDiameter,left+right+1)
-      return 1+max(left,right)
+            left = depth(currentNode.left)
+            right = depth(currentNode.right)
 
-    depth(root)
-    return self.treeDiameter
-    
+            self.treeDiameter = max(self.treeDiameter, left + right + 1)
+            return 1 + max(left, right)
 
-    
-  
+        depth(root)
+        return self.treeDiameter
+
 
 def main():
-  treeDiameter = TreeDiameter()
-  root = TreeNode(1)
-  root.left = TreeNode(2)
-  root.right = TreeNode(3)
-  root.left.left = TreeNode(4)
-  root.right.left = TreeNode(5)
-  root.right.right = TreeNode(6)
-  print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
-  root.left.left = None
-  root.right.left.left = TreeNode(7)
-  root.right.left.right = TreeNode(8)
-  root.right.right.left = TreeNode(9)
-  root.right.left.right.left = TreeNode(10)
-  root.right.right.left.left = TreeNode(11)
-  print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+    treeDiameter = TreeDiameter()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(6)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+    root.left.left = None
+    root.right.left.left = TreeNode(7)
+    root.right.left.right = TreeNode(8)
+    root.right.right.left = TreeNode(9)
+    root.right.left.right.left = TreeNode(10)
+    root.right.right.left.left = TreeNode(11)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
 
 
 main()
 
 
-
-#answer
+# answer
 class TreeNode:
-  def __init__(self, val, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 class TreeDiameter:
 
-  def __init__(self):
-    self.treeDiameter = 0
+    def __init__(self):
+        self.treeDiameter = 0
 
-  def find_diameter(self, root):
-    self.calculate_height(root)
-    return self.treeDiameter
+    def find_diameter(self, root):
+        self.calculate_height(root)
+        return self.treeDiameter
 
-  def calculate_height(self, currentNode):
-    if currentNode is None:
-      return 0
+    def calculate_height(self, currentNode):
+        if not currentNode:
+            return 0
 
-    leftTreeHeight = self.calculate_height(currentNode.left)
-    rightTreeHeight = self.calculate_height(currentNode.right)
+        leftTreeHeight = self.calculate_height(currentNode.left)
+        rightTreeHeight = self.calculate_height(currentNode.right)
 
-    # diameter at the current node will be equal to the height of left subtree +
-    # the height of right sub-trees + '1' for the current node
-    diameter = leftTreeHeight + rightTreeHeight + 1
+        diameter = leftTreeHeight + rightTreeHeight + 1
+        self.treeDiameter = max(self.treeDiameter, diameter)
 
-    # update the global tree diameter
-    self.treeDiameter = max(self.treeDiameter, diameter)
-
-    # height of the current node will be equal to the maximum of the hights of
-    # left or right subtrees plus '1' for the current node
-    return max(leftTreeHeight, rightTreeHeight) + 1
+        return max(leftTreeHeight, rightTreeHeight) + 1
 
 
 def main():
-  treeDiameter = TreeDiameter()
-  root = TreeNode(1)
-  root.left = TreeNode(2)
-  root.right = TreeNode(3)
-  root.left.left = TreeNode(4)
-  root.right.left = TreeNode(5)
-  root.right.right = TreeNode(6)
-  print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
-  root.left.left = None
-  root.right.left.left = TreeNode(7)
-  root.right.left.right = TreeNode(8)
-  root.right.right.left = TreeNode(9)
-  root.right.left.right.left = TreeNode(10)
-  root.right.right.left.left = TreeNode(11)
-  print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+    treeDiameter = TreeDiameter()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.right.left = TreeNode(5)
+    root.right.right = TreeNode(6)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
+    root.left.left = None
+    root.right.left.left = TreeNode(7)
+    root.right.left.right = TreeNode(8)
+    root.right.right.left = TreeNode(9)
+    root.right.left.right.left = TreeNode(10)
+    root.right.right.left.left = TreeNode(11)
+    print("Tree Diameter: " + str(treeDiameter.find_diameter(root)))
 
 
 main()
-
-
 
 '''
 Time complexity 
