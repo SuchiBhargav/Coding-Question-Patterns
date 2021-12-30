cffece875bb26e627506543cb98b454981403f3b
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2021-12-30 14:22:02 +0300

    Completed Subset Questions

--- 10. Pattern Subsets/Problem Challenge 1 - Evaluate Expression (hard).py
+++ 10. Pattern Subsets/Problem Challenge 1 - Evaluate Expression (hard).py
@@ -19,89 +19,95 @@ Output: 8, -12, 7, -7, -3
 Explanation: 2*(3-(4-5)) => 8, 2*(3-4-5) => -12, 2*3-(4-5) => 7, 2*(3-4)-5 => -7, (2*3)-4-5 => -3
 '''
 
-#mycode
+
+# mycode
 def diff_ways_to_evaluate_expression(input):
-  result = []
-  # TODO: Write your code here
+    return diff_ways_to_evaluate_expression_rec({}, input)
 
-  if '+' not in input and '-' not in input and '*' not in input:
-    result.append(input)
-  else:
-    for i in range(len(input)):
-      if not input[i].isdigit():
-        left = diff_ways_to_evaluate_expression(input[0:i])
-        right = diff_ways_to_evaluate_expression(input[i+1:])
 
-        for j in left:
-          for k in right:
-            if input[i] == '+':
-              result.append(int(j)+int(k))
-            if input[i] == '*':
-              result.append(int(j)*int(k))
-            if input[i] == '-':
-              result.append(int(j)-int(k))
-  return result
+def diff_ways_to_evaluate_expression_rec(memo, input):
+    if input in memo:
+        return memo[input]
+
+    result = []
+    # base case: if the input string is a number
+    if '+' not in input and '-' not in input and '*' not in input:
+        result.append(int(input))
+    else:
+        for i in range(0, len(input)):
+            char = input[i]
+            if not char.isdigit():
+                # break the equation here into two parts and make recursively calls
+                leftParts = diff_ways_to_evaluate_expression_rec(memo, input[0:i])
+                rightParts = diff_ways_to_evaluate_expression_rec(memo, input[i+1:])
+                for part1 in leftParts:
+                    for part2 in rightParts:
+                        if char == '+':
+                            result.append(part1 + part2)
+                        elif char == '-':
+                            result.append(part1 - part2)
+                        elif char == '*':
+                            result.append(part1 * part2)
+
+    memo[input] = result
+    return result
 
 
 def main():
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("1+2*3")))
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("1+2*3")))
 
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("2*3-4-5")))
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("2*3-4-5")))
 
 
 main()
 
 
-
-
-#answer
+# answer
 def diff_ways_to_evaluate_expression(input):
-  return diff_ways_to_evaluate_expression_rec({}, input)
+    return diff_ways_to_evaluate_expression_rec({}, input)
 
 
 def diff_ways_to_evaluate_expression_rec(map, input):
-  if input in map:
-    return map[input]
+    if input in map:
+        return map[input]
 
-  result = []
-  # base case: if the input string is a number, parse and return it.
-  if '+' not in input and '-' not in input and '*' not in input:
-    result.append(int(input))
-  else:
-    for i in range(0, len(input)):
-      char = input[i]
-      if not char.isdigit():
-        # break the equation here into two parts and make recursively calls
-        leftParts = diff_ways_to_evaluate_expression_rec(
-          map, input[0:i])
-        rightParts = diff_ways_to_evaluate_expression_rec(
-          map, input[i+1:])
-        for part1 in leftParts:
-          for part2 in rightParts:
-            if char == '+':
-              result.append(part1 + part2)
-            elif char == '-':
-              result.append(part1 - part2)
-            elif char == '*':
-              result.append(part1 * part2)
+    result = []
+    # base case: if the input string is a number, parse and return it.
+    if '+' not in input and '-' not in input and '*' not in input:
+        result.append(int(input))
+    else:
+        for i in range(0, len(input)):
+            char = input[i]
+            if not char.isdigit():
+                # break the equation here into two parts and make recursively calls
+                leftParts = diff_ways_to_evaluate_expression_rec(
+                    map, input[0:i])
+                rightParts = diff_ways_to_evaluate_expression_rec(
+                    map, input[i + 1:])
+                for part1 in leftParts:
+                    for part2 in rightParts:
+                        if char == '+':
+                            result.append(part1 + part2)
+                        elif char == '-':
+                            result.append(part1 - part2)
+                        elif char == '*':
+                            result.append(part1 * part2)
 
-  map[input] = result
-  return result
+    map[input] = result
+    return result
 
 
 def main():
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("1+2*3")))
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("1+2*3")))
 
-  print("Expression evaluations: " +
-        str(diff_ways_to_evaluate_expression("2*3-4-5")))
+    print("Expression evaluations: " +
+          str(diff_ways_to_evaluate_expression("2*3-4-5")))
 
 
 main()
-
-
 
 '''
 Time complexity 
--- 10. Pattern Subsets/Problem Challenge 3 - Count of Structurally Unique Binary Search Trees (hard).py
+++ 10. Pattern Subsets/Problem Challenge 3 - Count of Structurally Unique Binary Search Trees (hard).py
@@ -19,73 +19,71 @@ Explanation: There will be 5 unique BSTs that can store numbers from 1 to 5.
 '''
 
 
-#mycode
+# mycode
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 
 def count_trees(n):
-  if n < 0:
-    return -1
-  if n <=1 :
-    return 1
+    if n < 0:
+        return -1
+    if n <= 1:
+        return 1
 
-  count = 0  
-  for i  in range(1,n+1):
-    left = count_trees(i-1)
-    right = count_trees(n-i)
-    count += left * right
-  return count
+    count = 0
+    for i in range(1, n + 1):
+        left = count_trees(i - 1)
+        right = count_trees(n - i)
+        count += left * right
+    return count
 
 
 def main():
-  print("Total trees: " + str(count_trees(2)))
-  print("Total trees: " + str(count_trees(3)))
+    print("Total trees: " + str(count_trees(2)))
+    print("Total trees: " + str(count_trees(3)))
 
 
 main()
 
 
-#answer
+# answer
 class TreeNode:
-  def __init__(self, val):
-    self.val = val
-    self.left = None
-    self.right = None
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
 
 def count_trees(n):
-  return count_trees_rec({}, n)
+    return count_trees_rec({}, n)
 
 
 def count_trees_rec(map, n):
-  if n in map:
-    return map[n]
+    if n in map:
+        return map[n]
 
-  if n <= 1:
-    return 1
-  count = 0
-  for i in range(1, n+1):
-    # making 'i' the root of the tree
-    countOfLeftSubtrees = count_trees_rec(map, i - 1)
-    countOfRightSubtrees = count_trees_rec(map, n - i)
-    count += (countOfLeftSubtrees * countOfRightSubtrees)
+    if n <= 1:
+        return 1
+    count = 0
+    for i in range(1, n + 1):
+        # making 'i' the root of the tree
+        countOfLeftSubtrees = count_trees_rec(map, i - 1)
+        countOfRightSubtrees = count_trees_rec(map, n - i)
+        count += (countOfLeftSubtrees * countOfRightSubtrees)
 
-  map[n] = count
-  return count
+    map[n] = count
+    return count
 
 
 def main():
-  print("Total trees: " + str(count_trees(2)))
-  print("Total trees: " + str(count_trees(3)))
+    print("Total trees: " + str(count_trees(2)))
+    print("Total trees: " + str(count_trees(3)))
 
 
 main()
-
-
 
 '''
 Time complexity #
