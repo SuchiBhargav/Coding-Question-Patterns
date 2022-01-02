86d24f022c2d204a667d68ca31dd3dbad266d557
Yasin Aydın <yasinaydin@sabanciuniv.edu>
2022-01-02 14:23:14 +0300

    Completed 0/1 Knapsack Questions

--- 15. Pattern  01 Knapsack (Dynamic Programming)/3 - Subset Sum (medium).py
+++ 15. Pattern  01 Knapsack (Dynamic Programming)/3 - Subset Sum (medium).py
@@ -21,13 +21,41 @@ The given set does not have any subset whose sum is equal to '6'.
 
 def can_partition(num, sum):
     n = len(num)
-    dp = [False for x in range(sum+1)]
+    dp = [[True if x == 0 else False for x in range(sum + 1)] for y in range(n)]
+
+    # only first number in subset
+    dp[0][num[0]] = True
+
+    # process all subsets for all sums
+    for i in range(1, n):
+        for s in range(1, sum + 1):
+            if dp[i - 1][s]:  # if we can get the sum 's' without the number at index 'i'
+                dp[i][s] = True
+            elif s >= num[i]:
+                # else include the number and see if we can find a subset to get the remaining sum
+                dp[i][s] = dp[i - 1][s - num[i]]
+
+    return dp[n - 1][sum]
+
+
+def main():
+    print("Can partition: " + str(can_partition([1, 2, 3, 7], 6)))
+    print("Can partition: " + str(can_partition([1, 2, 7, 1, 5], 10)))
+    print("Can partition: " + str(can_partition([1, 3, 4, 8], 6)))
+
+
+main()
+
+
+def can_partition(num, sum):
+    n = len(num)
+    dp = [False for x in range(sum + 1)]
 
     # handle sum=0, as we can always have '0' sum with an empty set
     dp[0] = True
 
     # with only one number, we can have a subset only when the required sum is equal to its value
-    for s in range(1, sum+1):
+    for s in range(1, sum + 1):
         dp[s] = num[0] == s
 
     # process all subsets for all sums
@@ -49,7 +77,6 @@ def main():
 
 
 main()
-
 
 '''
 Time and Space complexity 
--- 15. Pattern  01 Knapsack (Dynamic Programming)/Problem Challenge 2 - Target Sum (hard).py
+++ 15. Pattern  01 Knapsack (Dynamic Programming)/Problem Challenge 2 - Target Sum (hard).py
@@ -16,71 +16,92 @@ Explanation: The given set has '2' ways to make a sum of '9': {+1+2+7-1} & {-1+2
 '''
 
 
-#mycode
+# mycode
 def find_target_subsets(num, s):
-  dp = [[0 for x in range(2*sum(num)+1)] for y in range(len(num))]
-  len_s = sum(num) - s
+    totalSum = sum(num)
 
-  for i in range(2*sum(num)+1):
-    if i - len_s == num[0]:
-      dp[0][i] = 1
-    if i - len_s == -num[0]:
-      dp[0][i] = 1
-  
-  for i in range(1,len(num)):
-    for j in range(1,2*sum(num)+1):
-      if j - num[i] >= 0:
-        dp[i][j] += dp[i-1][j - num[i]]
-      if j + num[i] <= 2*sum(num):
-        dp[i][j] += dp[i-1][j + num[i]]
-  return dp[len(num)-1][s+len_s]
+    # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s + totalSum) / 2'
+    if totalSum < s or (s + totalSum) % 2 == 1:
+        return 0
 
-
-
-
-#answer
-def find_target_subsets(num, s):
-  totalSum = sum(num)
-
-  # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s + totalSum) / 2'
-  if totalSum < s or (s + totalSum) % 2 == 1:
-    return 0
-
-  return count_subsets(num, (s + totalSum) // 2)
+    return count_subsets(num, (s + totalSum) // 2)
 
 
 # this function is exactly similar to what we have in 'Count of Subset Sum' problem.
 def count_subsets(num, s):
-  n = len(num)
-  dp = [[0 for x in range(s+1)] for y in range(n)]
+    n = len(num)
+    dp = [[0 for x in range(s + 1)] for y in range(n)]
 
-  # populate the sum = 0 columns, as we will always have an empty set for zero sum
-  for i in range(0, n):
-    dp[i][0] = 1
+    # populate the sum = 0 columns, as we will always have an empty set for zero sum
+    for i in range(0, n):
+        dp[i][0] = 1
 
-  # with only one number, we can form a subset only when the required sum is
-  # equal to the number
-  for s in range(1, s+1):
-    dp[0][s] = 1 if num[0] == s else 0
+    # with only one number, we can form a subset only when the required sum is
+    # equal to the number
+    for s in range(1, s + 1):
+        dp[0][s] = 1 if num[0] == s else 0
 
-  # process all subsets for all sums
-  for i in range(1, n):
-    for s in range(1, s+1):
-      dp[i][s] = dp[i - 1][s]
-      if s >= num[i]:
-        dp[i][s] += dp[i - 1][s - num[i]]
+    # process all subsets for all sums
+    for i in range(1, n):
+        for s in range(1, s + 1):
+            dp[i][s] = dp[i - 1][s]
+            if s >= num[i]:
+                dp[i][s] += dp[i - 1][s - num[i]]
 
-  # the bottom-right corner will have our answer.
-  return dp[n - 1][s]
+    # the bottom-right corner will have our answer.
+    return dp[n - 1][s]
 
 
 def main():
-  print("Total ways: " + str(find_target_subsets([1, 1, 2, 3], 1)))
-  print("Total ways: " + str(find_target_subsets([1, 2, 7, 1], 9)))
+    print("Total ways: " + str(find_target_subsets([1, 1, 2, 3], 1)))
+    print("Total ways: " + str(find_target_subsets([1, 2, 7, 1], 9)))
 
 
 main()
 
+
+# answer
+def find_target_subsets(num, s):
+    totalSum = sum(num)
+
+    # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s + totalSum) / 2'
+    if totalSum < s or (s + totalSum) % 2 == 1:
+        return 0
+
+    return count_subsets(num, (s + totalSum) // 2)
+
+
+# this function is exactly similar to what we have in 'Count of Subset Sum' problem.
+def count_subsets(num, s):
+    n = len(num)
+    dp = [[0 for x in range(s + 1)] for y in range(n)]
+
+    # populate the sum = 0 columns, as we will always have an empty set for zero sum
+    for i in range(0, n):
+        dp[i][0] = 1
+
+    # with only one number, we can form a subset only when the required sum is
+    # equal to the number
+    for s in range(1, s + 1):
+        dp[0][s] = 1 if num[0] == s else 0
+
+    # process all subsets for all sums
+    for i in range(1, n):
+        for s in range(1, s + 1):
+            dp[i][s] = dp[i - 1][s]
+            if s >= num[i]:
+                dp[i][s] += dp[i - 1][s - num[i]]
+
+    # the bottom-right corner will have our answer.
+    return dp[n - 1][s]
+
+
+def main():
+    print("Total ways: " + str(find_target_subsets([1, 1, 2, 3], 1)))
+    print("Total ways: " + str(find_target_subsets([1, 2, 7, 1], 9)))
+
+
+main()
 
 '''
 Time and Space complexity 
@@ -90,39 +111,38 @@ We can further improve the solution to use only O(S) space.
 '''
 
 
-
 def find_target_subsets(num, s):
-  totalSum = sum(num)
+    totalSum = sum(num)
 
-  # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s +totalSum) / 2'
-  if totalSum < s or (s + totalSum) % 2 == 1:
-    return 0
+    # if 's + totalSum' is odd, we can't find a subset with sum equal to '(s +totalSum) / 2'
+    if totalSum < s or (s + totalSum) % 2 == 1:
+        return 0
 
-  return count_subsets(num, (s + totalSum) // 2)
+    return count_subsets(num, (s + totalSum) // 2)
 
 
 # this function is exactly similar to what we have in 'Count of Subset Sum' problem
 def count_subsets(num, sum):
-  n = len(num)
-  dp = [0 for x in range(sum+1)]
-  dp[0] = 1
+    n = len(num)
+    dp = [0 for x in range(sum + 1)]
+    dp[0] = 1
 
-  # with only one number, we can form a subset only when the required sum is equal to the number
-  for s in range(1, sum+1):
-    dp[s] = 1 if num[0] == s else 0
+    # with only one number, we can form a subset only when the required sum is equal to the number
+    for s in range(1, sum + 1):
+        dp[s] = 1 if num[0] == s else 0
 
-  # process all subsets for all sums
-  for i in range(1, n):
-    for s in range(sum, -1, -1):
-      if s >= num[i]:
-        dp[s] += dp[s - num[i]]
+    # process all subsets for all sums
+    for i in range(1, n):
+        for s in range(sum, -1, -1):
+            if s >= num[i]:
+                dp[s] += dp[s - num[i]]
 
-  return dp[sum]
+    return dp[sum]
 
 
 def main():
-  print("Total ways: " + str(find_target_subsets([1, 1, 2, 3], 1)))
-  print("Total ways: " + str(find_target_subsets([1, 2, 7, 1], 9)))
+    print("Total ways: " + str(find_target_subsets([1, 1, 2, 3], 1)))
+    print("Total ways: " + str(find_target_subsets([1, 2, 7, 1], 9)))
 
 
 main()
